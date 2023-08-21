0d4007b186fb63404aa8dcd496fe176c59d165ec
rj <fwd@rj95.be>
2023-08-20 21:56:27 -0500

    spinlock
    - rename constant

--- spinlock.c
+++ spinlock.c
@@ -2,7 +2,7 @@
 #include <stdatomic.h>
 #include <time.h>
 
-#define SPIN_BEFORE_SLEEP_MAX 8
+#define SPINLOCK_NSPINS_BEFORE_SLEEP 8
 
 void init(spinlock_t lock[static 1]) {
   *lock = 0;
@@ -13,7 +13,7 @@ void lock(spinlock_t lock[static 1]) {
        atomic_load_explicit(lock, memory_order_acquire) ||
        atomic_exchange_explicit(lock, 1, memory_order_acquire);
        i++) {
-    if (SPIN_BEFORE_SLEEP_MAX == i) {
+    if (SPINLOCK_NSPINS_BEFORE_SLEEP == i) {
       static struct timespec ts = {.tv_sec = 0, .tv_nsec = 1};
       nanosleep(&ts, 0);
       i = 0;
