f6496d2440b6cd99100710f37e4b3f97980809e1
rj <fwd@rj95.be>
2023-08-22 00:02:26 -0500

    spinlock
    - move things around and rework init
    - doc to explain

--- spinlock.c
+++ spinlock.c
@@ -1,26 +1,8 @@
 #include "spinlock.h"
 #include <stdatomic.h>
-#include <time.h>
 
-#define SPINLOCK_NSPINS_BEFORE_SLEEP 8
-
-void init(spinlock_t lock[static 1]) {
+int init(spinlock_t *lock) {
+  if (!lock) return spinlock_error;
   *lock = 0;
-}
-
-void lock(spinlock_t lock[static 1]) {
-  for (register int i = 0;
-       atomic_load_explicit(lock, memory_order_acquire) ||
-       atomic_exchange_explicit(lock, 1, memory_order_acquire);
-       i++) {
-    if (SPINLOCK_NSPINS_BEFORE_SLEEP == i) {
-      static struct timespec ts = {.tv_sec = 0, .tv_nsec = 1};
-      nanosleep(&ts, 0);
-      i = 0;
-    }
-  }
-}
-
-void unlock(spinlock_t lock[static 1]) {
-  atomic_store_explicit(lock, 0, memory_order_release);
+  return ATOMIC_INT_LOCK_FREE ? spinlock_success_lf : spinlock_success_no_lf;
 }
--- spinlock.h
+++ spinlock.h
@@ -1,14 +1,31 @@
 #ifndef SPINLOCK_H
 #define SPINLOCK_H
 
-#include <assert.h>
 #include <stdatomic.h>
+#include <time.h>
+
+#define SPINLOCK_NSPINS_BEFORE_SLEEP 8
 
 typedef atomic_int spinlock_t;
-static_assert(ATOMIC_INT_LOCK_FREE, "atomic_int not lf");
 
-void init(spinlock_t lock[static 1]);
-void lock(spinlock_t lock[static 1]);
-void unlock(spinlock_t lock[static 1]);
+enum { spinlock_success_lf, spinlock_success_no_lf, spinlock_error };
+
+int init(spinlock_t *lock);
+
+static inline void lock(spinlock_t lock[static 1]) {
+  for (int i = 0; atomic_load_explicit(lock, memory_order_acquire) ||
+                  atomic_exchange_explicit(lock, 1, memory_order_acquire);
+       i++) {
+    if (SPINLOCK_NSPINS_BEFORE_SLEEP == i) {
+      static struct timespec ts = {.tv_sec = 0, .tv_nsec = 1};
+      nanosleep(&ts, 0);
+      i = 0;
+    }
+  }
+}
+
+static inline void unlock(spinlock_t lock[static 1]) {
+  atomic_store_explicit(lock, 0, memory_order_release);
+}
 
 #endif
