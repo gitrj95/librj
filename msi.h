458401b587497d7722783be4e9288eda6466c352
rj <fwd@rj95.be>
2023-09-12 14:03:53 -0500

    MSI refactor.
    - More asserts to clarify invariants.
    - Make indexes i32.

--- msi.h
+++ msi.h
@@ -4,18 +4,21 @@
 #include <assert.h>
 #include <stdint.h>
 
-static inline int64_t msi_next(int exp, uint64_t hash, int64_t i);
+static inline int32_t msi_next(int exp, uint64_t hash, int32_t i);
 
-static inline int64_t msi_init(int exp, uint64_t hash) {
-  return msi_next(exp, hash, hash & (((uint64_t)1 << 63) - 1));
+static inline int32_t msi_init(int exp, uint64_t hash) {
+  return msi_next(exp, hash, hash & 0xcfffffff);
 }
 
-static inline int64_t msi_next(int exp, uint64_t hash, int64_t i) {
-  assert(exp < 63 && "Length is bounded above by -1 + 2^63 and is required to "
-                     "be a power of 2.");
-  uint64_t m = ((uint64_t)1 << exp) - 1;
-  uint64_t step = (hash >> (64 - exp)) | 1;
-  return (i + step) & m;
+static inline int32_t msi_next(int exp, uint64_t hash, int32_t i) {
+  assert(exp > 0);
+  assert(exp < 31);
+  assert(i > -1);
+  uint32_t m = (1 << exp) - 1;
+  uint32_t s = ((hash >> 34) >> (30 - exp)) | 1;
+  assert(s % 2);
+  assert(s < (1 << exp));
+  return (i + s) & m;
 }
 
 #endif
