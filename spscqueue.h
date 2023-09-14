eb069a10189d6e4a1b251a07814f11021f176c11
rj <fwd@rj95.be>
2023-09-13 20:20:14 -0500

    SPSC cleanup.
    - Some documentation clarifications.

--- spscqueue.h
+++ spscqueue.h
@@ -44,13 +44,13 @@ traffic by significantly reducing the need to atomically load the
 others' pointers.
 
 What remains is how we make writes visible. It is clear that we must
-pair a release with an acquire when we copy the value into the buffer
-and publish it for the consumer to see. We can use the above trick to
-avoid needing to use an acquire barrier in the consumer in most
-cases. Namely, as the consumer's cached copy is behind the true writer
-pointer, the memory up to the cached copy must be visible to the
-consumer. If we need to reload the cached copy with the atomic writer
-pointer, at that point, we must load with acquire semantics to
+pair a release with an acquire when we copy a new value into the
+buffer and publish it for the consumer to see. We can use the above
+trick to avoid needing to use an acquire barrier in the consumer in
+most cases. Namely, as the consumer's cached copy is behind the true
+writer pointer, the memory up to the cached copy must be visible to
+the consumer. If we need to update the cached copy with the atomic
+writer pointer, at that point, we must load with acquire semantics to
 guarantee initialized memory of the cells between our cached copy and
 the update with the true value. The consumer will never advance ahead
 of the producer by the same argument.
@@ -59,15 +59,15 @@ Do we need any other barriers despite synchronization with multiple
 atomics? Actually, no. Intuitively, the consumer is not trying to make
 anything visible to the producer, as it's only updating something that
 the writer never stores-to, only loads-from. Formally, we only have
-two cases. The writes to the reader pointer are atomic, so we have,
-that, at minimum, that the load from the producer is indeterminately
-sequenced with the store from the consumer. If the load from the
-producer happens before the store from the consumer, the writer's
-cached reader pointer is behind, and we potentially may fail a write:
-harmless. If the store from the consumer happens before the load from
-the producer, the writer's cached reader pointer will be updated to a
-less stale value: the "correct" behavior. However the sequencing, we
-have valid behavior.
+two cases. The reader pointer is atomic, so we have, that, at minimum,
+that the load from the producer is indeterminately sequenced with the
+store from the consumer. If the load from the producer happens before
+the store from the consumer, the writer's cached reader pointer is
+behind, and we potentially may fail a write: harmless. If the store
+from the consumer happens before the load from the producer, the
+writer's cached reader pointer will be updated to a less stale value:
+the "correct" behavior. However the sequencing, we have valid
+behavior.
 
 In sum, we can reduce the miss rate and the barrier placement by
 virtue of symmetric, cached values that are guaranteed to be "behind",
