97a383323f0d30d8073d389f4162215ab38b9c05
rj <fwd@rj95.be>
2023-09-20 23:32:48 -0500

    Arena docs.

--- arena.h
+++ arena.h
@@ -1,3 +1,12 @@
+/*
+  Change the nature of the allocation problem. malloc/free are
+  extremely general. Arenas are trivial to compose with more complex
+  allocators and data structures, provide nice spatial locality via
+  linear allocation, and help untangle the lifetime mess. Memory
+  management in C is much more enjoyable:
+  https://www.rfleury.com/p/untangling-lifetimes-the-arena-allocator
+*/
+
 #ifndef ARENA_H
 #define ARENA_H
 
