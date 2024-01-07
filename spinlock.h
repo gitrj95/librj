5c6a064c5f3c2e4aa56c6187b5f33e8ec444c3c2
rj <fwd@rj95.be>
2024-01-07 12:16:46 -0600

    Duh...

--- spinlock.h
+++ spinlock.h
@@ -1,13 +1,14 @@
 /*
-  This is an implementation of an optimized spinlock. To avoid paying
-  the penalty for a RMW on each spin, we do a load first and only
-  attempt a write if the load works i.e. the lock appears
-  unlocked. There is no need to do any atomic comparisons, here, given
-  we have two states (0 or 1 WLOG). The nanosleep is a heuristic hack
-  sourced from Fedor Pikus. Without the sleep, the OS would think that
-  the waiter of a lock is doing lots of work, so it would give it more
-  time slices erroneously. Namely, a hardware `PAUSE' wouldn't
-  necessarily inform the kernel.
+  This is an implementation of an optimized spinlock that has
+  reasonable performance on a variety of hardware. To avoid paying the
+  penalty for a RMW on each spin, we do a load first and only attempt
+  a write if the load works i.e. the lock appears unlocked. There is
+  no need to do any atomic comparisons, here, given we have two states
+  (0 or 1 WLOG). The nanosleep is a heuristic hack sourced from Fedor
+  Pikus. Without the sleep, the OS would think that the waiter of a
+  lock is doing lots of work, so it would give it more time slices
+  erroneously. Namely, a hardware `PAUSE' wouldn't necessarily inform
+  the kernel.
 
   Spinlocks also compose nicely with other concurrency structures.
 */
@@ -25,7 +26,7 @@ typedef atomic_int spinlock;
 
 static inline void spin_lock(spinlock *lock) {
   assert(lock);
-  for (int i = 0; atomic_load_explicit(lock, memory_order_acquire) ||
+  for (int i = 0; atomic_load_explicit(lock, memory_order_relaxed) ||
                   atomic_exchange_explicit(lock, 1, memory_order_acquire);
        ++i) {
     if (8 == i) {
