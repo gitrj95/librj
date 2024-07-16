fb872285035ff17b98a4c9cd1cd4099e68e42395
rj <fwd@rj95.be>
2024-07-15 22:28:08 -0500

    MSI attr

--- msi.h
+++ msi.h
@@ -3,8 +3,9 @@
 
 #include "base.h"
 
+[[gnu::const]]
 static inline uint msi( short lg, ulong h, uint i ) {
-  uint w    = ( 1ul << lg ) - 1;
+  uint w    = ( 1 << lg ) - 1;
   uint step = ( ( h >> 32 ) >> ( 32 - lg ) ) | 1;
   return i + step & w;
 }
