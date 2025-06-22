489f09f3b601fd78be9c3cba292787db3bb8c18a
rj <fwd@rj95.be>
2025-06-21 23:55:11 -0500

    Less

--- base.h
+++ base.h
@@ -7,26 +7,20 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef unsigned long  ulong;
-typedef unsigned int   uint;
-typedef unsigned short ushort;
-typedef unsigned char  uchar;
-typedef signed char    schar;
+static_assert(CHAR_BIT == 8);
 
-static_assert( CHAR_BIT == 8 );
+static_assert(sizeof(long) == 8);
+static_assert(sizeof(int) == 4);
+static_assert(sizeof(short) == 2);
 
-static_assert( sizeof( long ) == 8 );
-static_assert( sizeof( int ) == 4 );
-static_assert( sizeof( short ) == 2 );
+static_assert(sizeof(long) == sizeof(size_t));
+static_assert(sizeof(long) == sizeof(intptr_t));
+static_assert(sizeof(long) == sizeof(ptrdiff_t));
 
-static_assert( sizeof( long ) == sizeof( size_t ) );
-static_assert( sizeof( long ) == sizeof( intptr_t ) );
-static_assert( sizeof( long ) == sizeof( ptrdiff_t ) );
-
-static_assert( ATOMIC_POINTER_LOCK_FREE == 2 );
-static_assert( ATOMIC_LONG_LOCK_FREE == 2 );
-static_assert( ATOMIC_INT_LOCK_FREE == 2 );
-static_assert( ATOMIC_SHORT_LOCK_FREE == 2 );
-static_assert( ATOMIC_CHAR_LOCK_FREE == 2 );
+static_assert(ATOMIC_POINTER_LOCK_FREE == 2);
+static_assert(ATOMIC_LONG_LOCK_FREE == 2);
+static_assert(ATOMIC_INT_LOCK_FREE == 2);
+static_assert(ATOMIC_SHORT_LOCK_FREE == 2);
+static_assert(ATOMIC_CHAR_LOCK_FREE == 2);
 
 #endif
